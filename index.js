f43fba0f3b30e766240ab531879743bb81bdefb9
Ershad-Alam-67 <ealam6732@gmail.com>
2023-05-18 23:43:57 +0530

    Using getElemtntById and getElementsByTag

---
+++ index.js
@@ -0,0 +1,5 @@
+const li = document.getElementById("i5");
+const li2 = document.getElementsByTagName("li");
+const lastLi = li2[li2.length - 1];
+li.style.backgroundColor = "green";
+lastLi.style.fontWeight = "bold";
