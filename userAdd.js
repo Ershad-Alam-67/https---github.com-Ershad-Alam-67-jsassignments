c86652a5064586ac2c74ace4fa5547677f503da9
Ershad-Alam-67 <ealam6732@gmail.com>
2023-05-21 16:32:49 +0530

    Edit Button

--- userAdd.js
+++ userAdd.js
@@ -11,13 +11,20 @@ function fun(e) {
   const ul = document.getElementById("ul");
   li.setAttribute("class", "list-group-item");
   const del = document.createElement("input");
-  del.setAttribute("class", "btn btn-primary delete");
+  del.setAttribute("class", "btn btn-danger delete");
 
   del.setAttribute("id", "delete");
   del.setAttribute("type", "button");
   del.setAttribute("value", "delete");
   del.setAttribute("onclick", "funDel(event)");
+  const edit = document.createElement("input");
+  edit.setAttribute("type", "button");
+  edit.setAttribute("value", "edit");
+  edit.setAttribute("class", "btn btn-primary edit");
+  edit.setAttribute("id", "edit");
+  edit.setAttribute("onclick", "editFun(event)");
   li.appendChild(del);
+  li.appendChild(edit);
   ul.appendChild(li);
 
   let obj = {
@@ -36,4 +43,15 @@ function funDel(event) {
   const ar = key.split("-");
   localStorage.removeItem(ar[1]);
   ul.removeChild(event.target.parentNode);
+}
+function editFun(event) {
+  const tar = event.target.parentNode;
+  const ar = tar.textContent.split("-");
+  document.getElementById("name").value = ar[0];
+  document.getElementById("email").value = ar[1];
+  document.getElementById("phone").value = ar[2];
+  const ul = document.getElementById("ul");
+  localStorage.removeItem(ar[1]);
+  ul.removeChild(event.target.parentNode);
+  console.log(ar);
 }
